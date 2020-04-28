511d70951646306b69c1e3d52b76f288d92bac3b
minxpan <61602133+minxpan@users.noreply.github.com>
2020-04-28 10:50:29 -0400

    Add files via upload

--- greenFuctionInverse_letter_A.m
+++ greenFuctionInverse_letter_A.m
@@ -5,31 +5,39 @@ close all;
 global lambda k AmpImage N M dW z;
 
 % Data of Amplitude on the Image Plane
-%15 by 15 lasers
-AmpImage = [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
-        0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
-        0 0 0 0 0 0 0 1 1 0 0 0 0 0 0
-        0 0 0 0 0 0 1 1 1 1 0 0 0 0 0
-        0 0 0 0 0 1 1 1 1 1 1 0 0 0 0
-        0 0 0 0 1 1 1 0 0 1 1 1 0 0 0
-        0 0 0 0 1 1 0 0 0 0 1 1 0 0 0
-        0 0 0 0 1 1 0 0 0 0 1 1 0 0 0
-        0 0 0 0 1 1 1 1 1 1 1 1 0 0 0
-        0 0 0 0 1 1 1 1 1 1 1 1 0 0 0
-        0 0 0 0 1 1 0 0 0 0 1 1 0 0 0
-        0 0 0 0 1 1 0 0 0 0 1 1 0 0 0
-        0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
-        0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
-        0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+%20 by 20 lasers
+AmpImage = [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+        0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+        0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+        0 0 0 0 0 0 0 0 1 1 1 1 0 0 0 0 0 0 0 0
+        0 0 0 0 0 0 0 1 1 1 1 1 1 0 0 0 0 0 0 0
+        0 0 0 0 0 0 1 1 0 0 0 0 1 1 0 0 0 0 0 0
+        0 0 0 0 0 1 1 0 0 0 0 0 0 1 1 0 0 0 0 0
+        0 0 0 0 1 1 0 0 0 0 0 0 0 0 1 1 0 0 0 0
+        0 0 0 0 1 1 0 0 0 0 0 0 0 0 1 1 0 0 0 0
+        0 0 0 0 1 1 0 0 0 0 0 0 0 0 1 1 0 0 0 0
+        0 0 0 0 1 1 0 0 0 0 0 0 0 0 1 1 0 0 0 0
+        0 0 0 0 1 1 0 0 0 0 0 0 0 0 1 1 0 0 0 0
+        0 0 0 0 1 1 1 1 1 1 1 1 1 1 1 1 0 0 0 0
+        0 0 0 0 1 1 0 0 0 0 0 0 0 0 1 1 0 0 0 0
+        0 0 0 0 1 1 0 0 0 0 0 0 0 0 1 1 0 0 0 0
+        0 0 0 0 1 1 0 0 0 0 0 0 0 0 1 1 0 0 0 0
+        0 0 0 0 1 1 0 0 0 0 0 0 0 0 1 1 0 0 0 0
+        0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+        0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+        0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+
         ];
 
+PhaseImage = (rand(20, 20) - 0.5);
+% AmpImage = AmpImage .* exp(1i * PhaseImage);
 figure;
-imagesc(AmpImage);
+imagesc(abs(AmpImage));
 colormap gray
 colorbar
 title('Image Plane(desired pattern)')
 
-dW = 10e-6; %Spacing between elements
+dW = 20e-6; %Spacing between elements
 z = 50e-6; % Distance of Image plane from the resonator plane
 
 lambda = 1.550e-6;
@@ -37,27 +45,101 @@ k = 2 * pi / lambda;
 [N, M] = size(AmpImage); % Image plane discretization size
 E_Sample = zeros(N, M); % initial an array to store sample image for calculation
 
-for ii = 1:N
+E_Sample = supperposition(E_Sample);
 
-    for jj = 1:M
+E_Sample = normalize(E_Sample);
 
-        E_Sample = E_Sample + Green(ii, jj);
-    end
-
-end
-
-% E_Sample = normalize(E_Sample);
 figure;
-imagesc(abs(E_Sample));
+imagesc(abs(E_Sample), [0 1]);
 colormap jet
 colorbar
-title('Sample Plane Intensity')
+title('Sample Plane Amplitude')
 
 figure;
 imagesc(angle(E_Sample));
 colormap jet
 colorbar
 title('Sample Plane Phase')
+
+E_Sample_Intensity = (abs(E_Sample)).^2;
+E_Sample_Intensity = Binarize(E_Sample_Intensity);
+figure;
+imagesc(E_Sample_Intensity);
+colormap jet
+colorbar
+title('Sample Plane Intensity')
+
+E_Sample_Phase = quantizePhase(angle(E_Sample));
+% E_Sample_Phase = (angle(E_Sample));
+figure;
+imagesc(E_Sample_Phase);
+colormap jet;
+colorbar;
+title('Sample Plane Quantized Phase');
+
+E_Sample_recovered = E_Sample_Intensity .* exp(-1i * E_Sample_Phase);
+E_Image_recovered = supperposition(E_Sample_recovered);
+E_Image_recovered = normalize(E_Image_recovered);
+figure;
+imagesc(abs(E_Image_recovered), [0.1 1]);
+colormap jet;
+colorbar;
+title('Recovered Image Plane Intensity');
+
+function E_Sample = supperposition(E_Sample)
+
+    [M, N] = size(E_Sample);
+
+    for ii = 1:M
+
+        for jj = 1:N
+
+            E_Sample = E_Sample + Green(ii, jj);
+        end
+
+    end
+
+end
+
+function quantizedPhase = quantizePhase(Array)
+
+    [M, N] = size(Array);
+    quantized = [-pi, -pi / 2, 0, pi / 2, pi]; % four level quantization
+
+    for ii = 1:M
+
+        for jj = 1:N
+            [~, idx] = min(abs(quantized - Array(ii, jj)));
+            Array(ii, jj) = quantized(idx);
+
+        end
+
+    end
+
+    quantizedPhase = Array;
+end
+
+function BinarizedArray = Binarize(Array)
+
+    [M, N] = size(Array);
+
+    for ii = 1:M
+
+        for jj = 1:N
+
+            if (Array(ii, jj) >= 0.25)% half amplitude
+                Array(ii, jj) = 1;
+            else
+                Array(ii, jj) = 0;
+            end
+
+        end
+
+    end
+
+    BinarizedArray = Array;
+
+end
 
 function E_Sample = Green(coordImageX, coordImageY)
 
@@ -84,13 +166,12 @@ function E_Sample = Green(coordImageX, coordImageY)
 end
 
 function normalizedIntensity = normalize(Original)
-    [N, M] = size(Original);
-    normalizedIntensity = zeros(N, M);
+    [M, N] = size(Original);
     Emax = max(max(abs(Original)));
 
-    for ii = 1:N
+    for ii = 1:M
 
-        for jj = 1:M
+        for jj = 1:N
 
             Original(ii, jj) = Original(ii, jj) / Emax;
 
